29db4d1d9dfe5722aed0638819933e94c5c6277e
drumboy02 <153551032+drumboy02@users.noreply.github.com>
2024-11-21 17:48:45 -0500

    added operator function and functions for each operation

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log('test')
+// add function
+function add(a, b) {
+    return a + b;
+}
+// subtract function
+function subtract(a, b) {
+    return a - b;
+}
+// multiply function
+function multiply(a, b) {
+    return a * b;
+}
+// divide function
+function divide(a, b) {
+    return a / b;
+}
+
+// create 3 variables for each part of operation
+let operandA;
+let operandB;
+let operator;
+
+// create operate function
+function operate(op, a, b) {
+    switch (op) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return "ERROR";
+    }
+}
+
+operandA = 4;
+operandB = 2;
+operator = "/";
+
+console.log(
+    "operate(): " + operate(operator, operandA, operandB)
+)
